f87f2ce2b9329bb8a6b31543d1933c07c6551424
Simon Leger <81974217+sileger@users.noreply.github.com>
2023-04-17 11:53:19 -0300

    Update ratecode.jl

--- ratecode.jl
+++ ratecode.jl
@@ -2,7 +2,6 @@ using LinearAlgebra, Plots, Distributions, Random, StableRNGs, Statistics
 include("utilities.jl")
 
 global seed = 1234
-Random.seed!(seed)
 
 xx1_noise = 0.01 # noise levels outside the range of 0.005 and 0.015 produce strange results
 pr = create_params(xx1_noise)
@@ -19,65 +18,81 @@ Tau = 200; Rise = 0.02; Max = 0.2 # for KNa Adapt
 spiking = false # set spiking model (true) or ratecode model (false)
 KNaAdapt = true 
 
-n = neuron(GbarE, GbarL,ErevE, ErevL, ErevK, Vm, Tau, Rise, Max)
+n = Vector{neuron}(undef, 2)
+fig = Vector(undef, 2)
+
+n[1] = neuron(GbarE, GbarL,ErevE, ErevL, ErevK, Vm, Tau, Rise, Max)
+n[2] = neuron(GbarE, GbarL,ErevE, ErevL, ErevK, Vm, Tau, Rise, Max)
 
 n_cycles = 300
 GeNoise = 0.02 # Noise for Ge
 
-GePlot = Vector{Float64}(undef, n_cycles)
-VmPlot = Vector{Float64}(undef, n_cycles)
-ΔVmPlot = Vector{Float64}(undef, n_cycles)
-GKNAPlot = Vector{Float64}(undef, n_cycles)
-ActPlot = Vector{Float64}(undef, n_cycles)
-G_theta_ePlot = Vector{Float64}(undef, n_cycles)
+GePlot = Matrix{Float64}(undef, n_cycles, 2)
+VmPlot = Matrix{Float64}(undef, n_cycles, 2)
+ΔVmPlot = Matrix{Float64}(undef, n_cycles, 2)
+GKNAPlot = Matrix{Float64}(undef, n_cycles, 2)
+ActPlot = Matrix{Float64}(undef, n_cycles, 2)
+G_theta_ePlot = Matrix{Float64}(undef, n_cycles, 2)
 
 
-for cycle in 1:n_cycles
-    if cycle > 20 && cycle < 180
-        n.Ge = n.GbarE + (randn() * GeNoise)
-    else
-        n.Ge = 0
-    end
-    
-    n.ΔVm = n.Ge*(n.ErevE - n.Vm) + n.Gl*(n.ErevL - n.Vm) + n.GKNA*(n.ErevK - n.Vm) # update current
-    n.Vm += n.ΔVm / 10 # change membrane potential based on current
-    
 
-    if !spiking
-        n.Act += abs(n.ΔVm) * (noisy_xx1(pr, n.Ge - compute_g_theta_e(n)) - n.Act) # Nxx1 activation function 
-        #n.Act += abs(n.ΔVm) * (sigmoid(n.Ge - compute_g_theta_e(n), 500) - n.Act) # Sigmoid activation function
-    end
-
-    if KNaAdapt && !spiking
-        n.GKNA += n.Act * n.Rise * (n.Max - n.GKNA) - (1/n.Tau * n.GKNA) # formula from O'Reily
-    end
-
-    if n.Vm > n.theta && spiking # spiking
-        n.Vm = 0.3
-        n.Act = 1
-        if KNaAdapt
-            n.GKNA += (n.Rise * (n.Max - n.GKNA))
+for i in 1:2
+    Random.seed!(seed)
+    for cycle in 1:n_cycles
+        if cycle > 20 && cycle < 180
+            n[i].Ge = n[i].GbarE + (randn() * GeNoise)
+        else
+            n[i].Ge = 0
         end
-    elseif KNaAdapt
-        n.GKNA -= ((1/n.Tau) * n.GKNA)
+        
+        n[i].ΔVm = n[i].Ge*(n[i].ErevE - n[i].Vm) + n[i].Gl*(n[i].ErevL - n[i].Vm) + n[i].GKNA*(n[i].ErevK - n[i].Vm) # update current
+        n[i].Vm += n[i].ΔVm / 10 # change membrane potential based on current
+        
+    
+        if !spiking
+            if i == 1
+                n[i].Act += abs(n[i].ΔVm) * (noisy_xx1(pr, n[i].Ge - compute_g_theta_e(n[i])) - n[i].Act) # Nxx1 activation function 
+            else
+                n[i].Act += abs(n[i].ΔVm) * (sigmoid(n[i].Ge - compute_g_theta_e(n[i]), 500) * 0.8 - n[i].Act) # Sigmoid activation function
+            end
+        end
+    
+        if KNaAdapt && !spiking
+            n[i].GKNA += n[i].Act * n[i].Rise * (n[i].Max - n[i].GKNA) - (1/n[i].Tau * n[i].GKNA) # formula from O'Reily
+        end
+    
+        if n[i].Vm > n[i].theta && spiking # spiking
+            n[i].Vm = 0.3
+            n[i].Act = 1
+            if KNaAdapt
+                n[i].GKNA += (n[i].Rise * (n[i].Max - n[i].GKNA))
+            end
+        elseif KNaAdapt
+            n[i].GKNA -= ((1/n[i].Tau) * n[i].GKNA)
+        end
+    
+        
+        GePlot[cycle, i] = n[i].Ge
+        VmPlot[cycle, i] = n[i].Vm
+        ΔVmPlot[cycle, i] = n[i].ΔVm
+        GKNAPlot[cycle, i] = n[i].GKNA
+        ActPlot[cycle, i] = n[i].Act
+        G_theta_ePlot[cycle, i] = compute_g_theta_e(n[i])
+    
     end
 
-    
-    GePlot[cycle] = n.Ge
-    VmPlot[cycle] = n.Vm
-    ΔVmPlot[cycle] = n.ΔVm
-    GKNAPlot[cycle] = n.GKNA
-    ActPlot[cycle] = n.Act
-    G_theta_ePlot[cycle] = compute_g_theta_e(n)
+    fig[i] = plot(VmPlot[:,i], label="Vm")
+    fig[i] = plot!(GePlot[:,i], label="Ge")
+    fig[i] = plot!(ΔVmPlot[:,i], label="ΔVm")
+    fig[i] = plot!(GKNAPlot[:,i], label="GKNA")
+    fig[i] = plot!(ActPlot[:,i], label="Act")
+    fig[i] = plot!(G_theta_ePlot[:,i], label="GthetaE")
+    fig[i] = ylims!(-0.2, 1)
 
 end
 
-fig = plot(VmPlot, label="Vm")
-fig = plot!(GePlot, label="Ge")
-fig = plot!(ΔVmPlot, label="ΔVm")
-fig = plot!(GKNAPlot, label="GKNA")
-fig = plot!(ActPlot, label="Act")
-fig = plot!(G_theta_ePlot, label="GthetaE")
-fig = ylims!(-0.2, 1)
+compare = plot(GePlot[:,1], label="Ge")
+compare = plot!(ActPlot[:,1], label="Act nxx1")
+compare = plot!(ActPlot[:,2], label="Act Sigmoid")
 
 
